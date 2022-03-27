a427b3c3a96e4bfec5df715c0af150586085c173
DashrrYt <102350219+DashrrYt@users.noreply.github.com>
2022-03-27 13:56:17 +0800

    Add files via upload

--- mainapi.py
+++ mainapi.py
@@ -6,8 +6,13 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 import json
 import os
+<<<<<<< HEAD
+import praw
+siteurl = "dashrr.cf"
+=======
 
 siteurl = "127.0.0.1:5000"
+>>>>>>> 73ae7f9ce6b21138c7c2c5a998e4780de031976a
 
 with open("json/token.json") as f: 
   apitoken = json.load(f)
@@ -23,6 +28,12 @@ app.config["DEBUG"] = False
 @app.route('/', methods=['GET'])
 def home():
     return render_template("index.html")
+<<<<<<< HEAD
+@app.route("/donate")
+def donate():
+  return render_template("donate.html")
+=======
+>>>>>>> 73ae7f9ce6b21138c7c2c5a998e4780de031976a
 @app.errorhandler(404)
 def page_not_found(e):
     # note that we set the 404 status explicitly
@@ -30,11 +41,19 @@ def page_not_found(e):
 @app.route("/api/")
 def apiList(): 
   return render_template("apilist.html", siteurl=siteurl)
+<<<<<<< HEAD
+
+=======
+>>>>>>> 73ae7f9ce6b21138c7c2c5a998e4780de031976a
 @app.route('/api/v1/8ball', methods=['GET'])
 def api8ball(): 
   token = request.args.get("token")
   if token == "0":
+<<<<<<< HEAD
+    time.sleep(1)
+=======
     time.sleep(3)
+>>>>>>> 73ae7f9ce6b21138c7c2c5a998e4780de031976a
     return jsonify(random.choice(answer8ball))
   elif token==apikey:
     time.sleep(1)
@@ -43,5 +62,36 @@ def api8ball():
     return "No Token Passed"
   else:
     return "Invalid token has been passed"
+<<<<<<< HEAD
+
+reddit = praw.Reddit(
+  client_id = "cyAr-LQpdSWlmOSQ4IWAoA", 
+  client_secret = "Um421poK7wj1milKgiglP8K1CMd9yg", 
+  username = "DashrrYT", 
+  password = "Vien@123", 
+  user_agent = "Catpics by u/DashrrYT"
+)
+
+@app.route("/api/v1/cats", methods=["GET"])
+def cats(): 
+  subreddit = reddit.subreddit("catpics")
+  all_subs = []
+  top = subreddit.top(limit=420) # bot will choose between the top 250 memes
+  for submission in top:
+    all_subs.append(submission)
+  random_sub = random.choice(all_subs)
+  jsonContent = { "title": f"{random_sub.title}", "image_url": f"{random_sub.url}" }
+  token = request.args.get("token")
+  if token == 0: 
+    time.sleep(1)
+    return jsonify(jsonContent)
+  elif token==apikey: 
+    return jsonify(jsonContent)
+  elif token==None : 
+    return "No token passed"
+  else:
+    return "invalid token"
+=======
+>>>>>>> 73ae7f9ce6b21138c7c2c5a998e4780de031976a
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=False)
